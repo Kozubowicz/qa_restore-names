19490741cb850d118f20020084e116a64ba9c67c
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2024-09-27 16:32:58 +0200

    solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,88 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
-
-  it('should ', () => {
-
-  });
+  const { restoreNames } = require('./restoreNames');
 
   // write tests here
+  it(`should be declared`, () => {
+    expect(restoreNames).toBeInstanceOf(Function);
+  });
+
+  it(`should return nothing`, () => {
+    expect(restoreNames([])).toBeUndefined();
+  });
+
+  it(`should throw Error if fullName not exist`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+      },
+      {
+        lastName: 'Adams',
+      },
+    ];
+
+    expect(() => restoreNames(users)).toThrow();
+  });
+
+  it(`should restore firstName from fullName if it is undefined
+     or property do not exist`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ]);
+  });
+
+  it('should not change firtName it is already exist', () => {
+    const users = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual([
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ]);
+  });
 });
